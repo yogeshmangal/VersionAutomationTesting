39386537730d909adcf56b426c4ea3fbe2b7bc0d
Yogesh Mangal <67264564+yogeshmangal@users.noreply.github.com>
2024-11-03 01:45:37 +0530

    M

--- Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("This file is available in v0.2 tag and more. It is not available in v0.1 tag.");
-    }
-}
